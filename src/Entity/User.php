7466612ab78d6275b9785f08cdcc773f3ac989f4
Nefro <nefronix@gmail.com>
2018-08-13 22:20:50 +0300

    Decrease field size

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,14 +22,14 @@ class User implements UserInterface
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @ORM\Column(type="string", length=170, unique=true)
      * @Assert\NotBlank()
      * @Assert\Email()
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @ORM\Column(type="string", length=170, unique=true)
      * @Assert\NotBlank()
      */
     private $username;
