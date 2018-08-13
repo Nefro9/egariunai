4207650802ec45b15605135d8ede3ca9ce9e5ebf
Nefro <nefronix@gmail.com>
2018-08-13 22:44:16 +0300

    Add newsletter form

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
+use App\Form\NewsletterForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,10 +18,30 @@ use Symfony\Component\HttpFoundation\Response;
 class WebsiteController extends AbstractController
 {
     /**
-     * @Route("/",  methods={"GET"}, name="homepage")
+     * @Route("/",  methods={"GET", "POST"}, name="homepage")
+     * @param Request $request
+     *
+     * @param SessionInterface $session
+     *
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, SessionInterface $session): Response
     {
-        return $this->render('index.html.twig');
+        $newsletter = new Newsletter;
+        $form = $this->createForm(NewsletterForm::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newsletter);
+            $em->flush();
+
+            $session->set('newssletter', 'done');
+        }
+
+        return $this->render('index.html.twig', [
+            'newsletter' => $form->createView()
+        ]);
     }
 }
---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @UniqueEntity(fields="email", message="Toks el.paštas jau užregistruotas")
+ */
+class Newsletter
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=170, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
